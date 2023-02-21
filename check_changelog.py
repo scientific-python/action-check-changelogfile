5965a7ca7073620414299d925c8fc5cafedb5c8d
William Jamieson <wjamieson@stsci.edu>
2023-02-21 09:37:33 -0500

    Add `pre-commit-ci[bot]` support

--- check_changelog.py
+++ check_changelog.py
@@ -23,7 +23,7 @@ if 'skip-changelog-checks' in pr_labels:
 
 # Skip check if it is the backport bot
 pr_author = event['pull_request']['user']['login']
-if pr_author == 'meeseeksmachine':
+if pr_author in ['meeseeksmachine', 'pre-commit-ci[bot]']:
     print(f'Skipping changelog check for backport bot "{pr_author}"')
     sys.exit(0)
 
